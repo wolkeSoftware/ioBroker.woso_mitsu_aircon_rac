53963aac4a1bd3eb0fcc411623b90eaff611511b
wolkesoftware@googlemail.com <wolkesoftware@googlemail.com>
2023-01-06 17:03:28 +0100

    first function including data from mitsu

--- main.js
+++ main.js
@@ -8,9 +8,36 @@
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
 const AirconStatClass = require("./lib/AirconStat.js");
+const AirconCoderClass = require("./lib/AirconStatCoder.js");
+
+const KEY_AIRCON_ID = "airconId";
+const KEY_AIRCON_STAT = "airconStat";
+//const KEY_AUTO_HEATING = "autoHeating";
+//const KEY_LED_STAT = "ledStat";
+
+const OPERATORID = "d2bc4571-1cea-4858-b0f2-34c18bef1901";
+const TIMEZONE = "Europe/Berlin";
+const ARCO_PORT = 51443;
+const ARCO_DEVICEID = "18547566-315b-4941-bb9b-90cedef4bbb7";
+
+const COMMAND_DELETE_ACCOUNT_INFO = "deleteAccountInfo";
+const COMMAND_GET_DEVICE_INFO = "getDeviceInfo";
+const COMMAND_SET_AIRCON_STAT = "setAirconStat";
+const COMMAND_SET_NETWORK_INFO = "setNetworkInfo";
+const COMMAND_UPDATE_ACCOUNT_INFO = "updateAccountInfo";
+const COMMAND_GET_AIRCON_STAT = "getAirconStat";
+/*
+const COMMAND_SET_OPTION_SETTING = "setOptionSetting";
+const COMMAND_UPDATE_FIRMWARE = "updateFirmware";
+*/
+
+const delay = (delayInms) => {
+    return new Promise(resolve => setTimeout(resolve, delayInms));
+};
 
 // Load your modules here, e.g.:
-// const fs = require("fs");
+//const fs = require("fs");
+const axios = require("axios");
 
 class WosoMitsuAirconRac extends utils.Adapter {
 
@@ -27,10 +54,15 @@ class WosoMitsuAirconRac extends utils.Adapter {
         // this.on("objectChange", this.onObjectChange.bind(this));
         // this.on("message", this.onMessage.bind(this));
         this.on("unload", this.onUnload.bind(this));
-        console.log("woso::startup");
         this.AirconStat = new AirconStatClass();
-        console.log("woso::startup::2");
-        console.log(this.AirconStat);
+        this.AirconId = "";
+        this.lastAirconData = null;
+        this.lastResponse = {};
+        this.lastError = "";
+        this.acCoder = new AirconCoderClass();
+
+
+
     }
 
     /**
@@ -44,27 +76,21 @@ class WosoMitsuAirconRac extends utils.Adapter {
 
         // The adapters config (in the instance object everything under the attribute "native") is accessible via
         // this.config:
-        this.log.info("config ip: " + this.config.ip);
+        this.log.info("woso::config ip: " + this.config.ip);
+
+        await this.initIOBStates();
+
+        await this.register_airco();
+
+        await this.getDataFromMitsu();
+
+        await this.setIOBStates();
 
         /*
         For every state in the system there has to be also an object of type state
         Here a simple template for a boolean variable named "testVariable"
         Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
         */
-        await this.setObjectNotExistsAsync("testVariable", {
-            type: "state",
-            common: {
-                name: "testVariable",
-                type: "boolean",
-                role: "indicator",
-                read: true,
-                write: true,
-            },
-            native: {},
-        });
-
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates("testVariable");
         // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
         // this.subscribeStates("lights.*");
         // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
@@ -75,21 +101,282 @@ class WosoMitsuAirconRac extends utils.Adapter {
             you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
         */
         // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync("testVariable", true);
+
+        await this.setIOBStates();
 
         // same thing, but the value is flagged "ack"
         // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync("testVariable", { val: true, ack: true });
+        // await this.setStateAsync("testVariable", { val: true, ack: true });
 
         // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+        // await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
         // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync("admin", "iobroker");
-        this.log.info("check user admin pw iobroker: " + result);
+        //let result = await this.checkPasswordAsync("admin", "iobroker");
+        //this.log.info("check user admin pw iobroker: " + result);
 
-        result = await this.checkGroupAsync("admin", "admin");
-        this.log.info("check group user admin group admin: " + result);
+        //result = await this.checkGroupAsync("admin", "admin");
+        //this.log.info("check group user admin group admin: " + result);
+
+        //get data from aircon and start timer
+        this.startTimerAction();
+    }
+
+    async setIOBStates() {
+        await this.setStateAsync("inOperation", this.AirconStat.operation);
+        await this.setStateAsync("OperationMode", this.AirconStat.operationMode);
+        await this.setStateAsync("Airflow", this.AirconStat.airFlow);
+        await this.setStateAsync("ModelNo", this.AirconStat.modelNo);
+        await this.setStateAsync("Indoor-Temp", this.AirconStat.indoorTemp);
+        await this.setStateAsync("Outdoor-Temp", this.AirconStat.outdoorTemp);
+        await this.setStateAsync("Preset-Temp", this.AirconStat.presetTemp);
+        await this.setStateAsync("Winddirection LR", this.AirconStat.windDirectionLR);
+        await this.setStateAsync("Winddirection UD", this.AirconStat.windDirectionUD);
+        await this.setStateAsync("Auto-Heating", this.AirconStat.isAutoHeating);
+        await this.setStateAsync("Cool-Hot-Judge", this.AirconStat.coolHotJudge);
+        await this.setStateAsync("Electric", this.AirconStat.electric);
+        await this.setStateAsync("Entrust", this.AirconStat.entrust);
+        await this.setStateAsync("Error-Code", this.AirconStat.errorCode);
+        await this.setStateAsync("Self-Clean-Operation", this.AirconStat.isSelfCleanOperation);
+        await this.setStateAsync("Self-Clean-Reset", this.AirconStat.isSelfCleanReset);
+        await this.setStateAsync("Vacant", this.AirconStat.isVacantProperty);
+    }
+
+    async initIOBStates() {
+        await this.setObjectNotExistsAsync("inOperation", {
+            type: "state",
+            common: {
+                name: "inOperation",
+                type: "boolean",
+                role: "indicator",
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+        this.subscribeStates("inOperation");
+
+        await this.setObjectNotExistsAsync("OperationMode", {
+            type: "state",
+            common: {
+                name: "OperationMode",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        this.subscribeStates("OperationMode");
+
+        await this.setObjectNotExistsAsync("Airflow", {
+            type: "state",
+            common: {
+                name: "Airflow",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        this.subscribeStates("Airflow");
+
+        await this.setObjectNotExistsAsync("ModelNo", {
+            type: "state",
+            common: {
+                name: "ModelNo",
+                type: "string",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        this.subscribeStates("ModelNo");
+
+        await this.setObjectNotExistsAsync("Indoor-Temp", {
+            type: "state",
+            common: {
+                name: "Indoor-Temp",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Indoor-Temp");
+
+        await this.setObjectNotExistsAsync("Outdoor-Temp", {
+            type: "state",
+            common: {
+                name: "Outdoor-Temp",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Outdoor-Temp");
+
+        await this.setObjectNotExistsAsync("Preset-Temp", {
+            type: "state",
+            common: {
+                name: "Preset-Temp",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        this.subscribeStates("Preset-Temp");
+
+        await this.setObjectNotExistsAsync("Winddirection LR", {
+            type: "state",
+            common: {
+                name: "Winddirection LR",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        this.subscribeStates("Winddirection LR");
+
+        await this.setObjectNotExistsAsync("Winddirection UD", {
+            type: "state",
+            common: {
+                name: "Winddirection UD",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        this.subscribeStates("Winddirection UD");
+
+        await this.setObjectNotExistsAsync("Auto-Heating", {
+            type: "state",
+            common: {
+                name: "Auto-Heating",
+                type: "boolean",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Auto-Heating");
+
+        await this.setObjectNotExistsAsync("Cool-Hot-Judge", {
+            type: "state",
+            common: {
+                name: "Cool-Hot-Judge",
+                type: "boolean",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Cool-Hot-Judge");
+
+        await this.setObjectNotExistsAsync("Electric", {
+            type: "state",
+            common: {
+                name: "Electric",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Electric");
+
+        await this.setObjectNotExistsAsync("Entrust", {
+            type: "state",
+            common: {
+                name: "Entrust",
+                type: "boolean",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Entrust");
+
+        await this.setObjectNotExistsAsync("Error-Code", {
+            type: "state",
+            common: {
+                name: "Error-Code",
+                type: "string",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Error-Code");
+
+        await this.setObjectNotExistsAsync("Self-Clean-Operation", {
+            type: "state",
+            common: {
+                name: "Self-Clean-Operation",
+                type: "boolean",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Self-Clean-Operation");
+
+        await this.setObjectNotExistsAsync("Self-Clean-Reset", {
+            type: "state",
+            common: {
+                name: "Self-Clean-Reset",
+                type: "boolean",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Self-Clean-Reset");
+
+        await this.setObjectNotExistsAsync("Vacant", {
+            type: "state",
+            common: {
+                name: "Vacant",
+                type: "number",
+                role: "indicator",
+                read: true,
+                write: false,
+            },
+            native: {},
+        });
+        //this.subscribeStates("Vacant");
+    }
+
+    startTimer() {
+        this.timer = setTimeout(() =>this.startTimerAction(), 60000);
+    }
+
+    async startTimerAction() {
+        await this.getDataFromAircon();
+        this.startTimer();
+    }
+
+    async getDataFromAircon() {
+        this.log.info("next Timer");
     }
 
     /**
@@ -159,6 +446,152 @@ class WosoMitsuAirconRac extends utils.Adapter {
     //         }
     //     }
     // }
+
+    ////Data-Transfer-Functions
+
+
+    async _post(port, cmd, contents) {
+        await delay(2050);
+        const url = "http://"+this.config.ip+":"+ARCO_PORT+"/beaver/command/"+cmd;
+        const data = {
+            "apiVer": "1.0",
+            "command": cmd,
+            "deviceId": ARCO_DEVICEID,
+            "operatorId": OPERATORID,
+            "timestamp": Date.now(),
+        };
+        if (contents != "") {
+            data["contents"] = contents;
+        }
+
+        const ret = {};
+        ret.error="";
+        ret.response="";
+        ret.body="";
+        try {
+            const response = await axios.post(url, data, {
+                headers: {
+                    "Connection": "close",
+                    "Content-Type": "application/json;charset=UTF-8",
+                    "Access-Control-Allow-Origin": "*",
+                    "accept": "application/json",
+                }});
+            ret.response = response.data;
+            this.lastResponse = ret.response;
+            //log("RESULT:"+JSON.stringify(response.data));
+            //console.log(util.inspect(response.data, { showHidden: false, depth: null }));
+        } catch (error) {
+            ret.error = error;
+            console.error(`Could not get Data: ${error}`);
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+        }
+        return ret;
+    }
+
+    async getInfoFromMitsu() {
+        let ret = {};
+        ret.error = "-1";
+
+        try {
+            ret = await this._post(COMMAND_GET_DEVICE_INFO);
+            ret = ret.response.contents;
+        } catch (error) {
+            this.log.error(`Could not get Data: ${error}`);
+            ret.error = error;
+        }
+        return ret;
+    }
+
+
+    async sendDataToMitsu(airco_id, command) {
+        let ret = {};
+        ret.error=-1;
+        //    var options = {KEY_AIRCON_ID: airco_id, "autoHeating" : 0, "ledStat" : 0,  "airconStat": command};
+        const options = { KEY_AIRCON_ID: ARCO_DEVICEID, KEY_AIRCON_STAT: command};
+
+
+        try {
+            ret = await this._post(COMMAND_SET_AIRCON_STAT, options);
+        } catch (error) {
+            this.log.error(`Could not get Data: ${error}`);
+        }
+
+        return ret;
+    }
+
+
+    async update_account_info(airco_id, time_zone) {
+        //Update the account info on the airco (sets to operator id of the device)
+        const contents = {
+            "accountId": OPERATORID,
+            "remote": 0,
+            "timezone": time_zone,
+            KEY_AIRCON_ID: ARCO_DEVICEID
+        };
+        return await this._post(COMMAND_UPDATE_ACCOUNT_INFO, contents);
+    }
+
+
+    async delete_account_info() {
+        this.log.info(KEY_AIRCON_ID);this.log.info(KEY_AIRCON_STAT);
+        //Update the account info on the airco (sets to operator id of the device)
+        const contents = {
+            "accountId": OPERATORID,
+            KEY_AIRCON_ID: ARCO_DEVICEID
+        };
+        return await this._post(COMMAND_DELETE_ACCOUNT_INFO, contents);
+    }
+
+    async set_network_info(airco_id) {
+        //Update the account info on the airco (sets to operator id of the device)
+        const contents = {
+            "ssid": airco_id,
+            "netPass": "192.168.178.12"
+        };
+        return await this._post(COMMAND_SET_NETWORK_INFO, contents);
+    }
+
+    async register_airco() {
+        //this.log.info("regData:"+JSON.stringify(AirconItem));
+        this.lastAirconData = await this.getInfoFromMitsu();
+        this.AirconId = this.lastAirconData.airconId;
+        //this.log.info("regData:"+JSON.stringify(AirconItem.airconData));
+        //var res = await delete_account_info(AirconItem.airconData.airconId);exit();
+        await this.update_account_info(this.AirconId, TIMEZONE);
+        //this.log.info("regres:"+res.response.result);
+
+        /*
+        //following Code is from JAVA, but no set_network is working and its not part of the python-scripts
+        if (res.response.result == 0) {
+            const res2 = await this.set_network_info(AirconItem.airconData.airconId, AirconItem.ip);
+        }
+        return AirconItem;
+
+         */
+    }
+
+    async getDataFromMitsu() {
+        let ret = {};
+        ret.error="-1";
+        const contents = {
+            KEY_AIRCON_ID: ARCO_DEVICEID
+        };
+        try {
+            ret = await this._post(COMMAND_GET_AIRCON_STAT, contents);
+            this.acCoder.fromBase64(this.AirconStat, ret.contents.airconStat);
+        } catch (error) {
+            this.log.error(`Could not get Data: ${error}`);
+            ret.error=error;
+        }
+        return ret;
+    }
+
+    ////End of Data-Transfer-Functions
+
 
 }
 
